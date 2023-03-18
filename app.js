8393f581b63c8c5a5faadf151e629f9866e06a5d
unknown <jjatuah@gmail.com>
2023-03-18 21:25:30 +0100

    grouped all instructions

--- app.js
+++ app.js
@@ -8,24 +8,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const session = require("express-session");
+const {initial, menu, invalidSelection, burgerOrder, shawarmaOrder, meatPierOrder, pizzaOrder, friesOrder} = require("./utils/instructions")
 
 const PORT = process.env.PORT || 7000
 
-const initial =["Select 1 to Place an order", "Select 98 to see order history", "Select 97 to see current order", "Select 0 to cancel order"]
-
-const menu = ["Select 2 to order a burger at $20", "Select 3 to order Shawarma at $40", "Select 4 to order Meat Pie at $10", "Select 5 to order Pizza at $100", "Select 6 to order our special fries $50"]
-
-const invalidSelection = ["Invalid Selection"]
-
-const burgerOrder = ["Burger orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
-
-const shawarmaOrder = ["Shawarma orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
-
-const meatPierOrder = ["MeatPie orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
-
-const pizzaOrder = ["Pizza orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
-
-const friesOrder = ["Fries orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
 //setting express session middleware
 const sessionMiddleware = session({
