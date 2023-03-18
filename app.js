4879ecb9924bf001ee380595bdbbb40880a57d22
unknown <jjatuah@gmail.com>
2023-03-18 14:53:45 +0100

    implemented all functionalities

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const friesOrder = ["Fries added to cart", "Select 90 to return to the main menu
 //Setting static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-let order =  {};
 let cart = 
   {
     burger: {
@@ -39,32 +38,32 @@ let cart =
     },
     shawarma: {
       quantity: 0,
-      price: 0,
+      price: 40,
       cost: 0
     },
     meatPie: {
       quantity: 0,
-      price: 0,
+      price: 10,
       cost: 0
     },
     pizza: {
       quantity: 0,
-      price: 0,
+      price: 100,
       cost: 0
     },
     fries: {
       quantity: 0,
-      price: 0,
+      price: 50,
       cost: 0
     },
   }
 
-  // let checkout = ["Cart Summary", `${cart.burger.quantity} bBurger ordered || cost ${cart.burger.cost} ` ]
+let checkout = ["Cart Summary","0 Burger ordered || cost 0","0 Shawarma ordered || cost 0","0 Meat Pie ordered || cost 0","0 Pizza ordered || cost 0","0 Fries ordered || cost 0"]; 
 
-let checkout = ["Cart Summary"]; 
+let curOrder = ["Current Order", "No Order Available"];
 
-const cartUpdate = (quantity, cost) => {
-  let checkoutUpdate = `${quantity} bBurger ordered || cost ${cost} `
+const cartUpdate = (quantity, cost, product) => {
+  let checkoutUpdate = `${quantity} ${product} ordered || cost ${cost} `
   return checkoutUpdate;
 }
 
@@ -88,22 +87,44 @@ io.on("connection", socket => {
       socket.emit("message", burgerOrder)
       cart.burger.quantity += 1
       cart.burger.cost += cart.burger.price
-      checkout[1] = cartUpdate(cart.burger.quantity, cart.burger.cost)
+      checkout[1] = cartUpdate(cart.burger.quantity, cart.burger.cost, "burger")
+      curOrder[1] = cartUpdate(1, 20, "burger")
     }
     else if(spacesRemoved === "3") {
       socket.emit("message", shawarmaOrder)
+      cart.shawarma.quantity += 1
+      cart.shawarma.cost += cart.shawarma.price
+      checkout[2] = cartUpdate(cart.shawarma.quantity, cart.shawarma.cost, "Shawarma")
+      curOrder[1] = cartUpdate(1, 40, "Shawarma")      
     }
     else if(spacesRemoved === "4") {
       socket.emit("message", meatPierOrder)
+      cart.meatPie.quantity += 1
+      cart.meatPie.cost += cart.meatPie.price
+      checkout[3] = cartUpdate(cart.meatPie.quantity, cart.meatPie.cost, "MeatPie")
+      curOrder[1] = cartUpdate(1, 10, "MeatPie")
     }
     else if(spacesRemoved === "5") {
       socket.emit("message", pizzaOrder)
+      cart.pizza.quantity += 1
+      cart.pizza.cost += cart.pizza.price
+      checkout[4] = cartUpdate(cart.pizza.quantity, cart.pizza.cost, "Pizza")
+      curOrder[1] = cartUpdate(1, 100, "Pizza")
     }
     else if(spacesRemoved === "6") {
       socket.emit("message", friesOrder)
+      cart.fries.quantity += 1
+      cart.fries.cost += cart.fries.price
+      checkout[5] = cartUpdate(cart.fries.quantity, cart.fries.cost, "Fries")
+      curOrder[1] = cartUpdate(1, 50, "Fries")
     }else if(spacesRemoved === "99") {
       socket.emit("message", checkout)
-      console.log(cart);
+    }else if(spacesRemoved === "97") {
+      socket.emit("message", curOrder)
+    }
+    else if(spacesRemoved === "0") {
+      curOrder[1] = "No Order Available"
+      socket.emit("message", curOrder)
     } else {
       socket.emit("message", invalidSelection)
     }
