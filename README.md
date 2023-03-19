6c41819e39ab8f4e6b4d145de1e7d176d067ab72
unknown <jjatuah@gmail.com>
2023-03-19 01:28:46 +0100

    Added README.md file

---
+++ README.md
@@ -0,0 +1,55 @@
+# Restaurant Chatbot
+
+This is a chatbot for a restaurant built using Node Js
+
+## Base URL
+
+- https://foodie-chatbot.onrender.com/
+
+## Setup
+
+- Install NodeJS
+- pull this repo
+- update .env
+- run `npm install`
+- run `node app.js`
+- load localhost on your browser
+
+## Instructions to use the Chatbot
+
+1. Enter the the required number based on the instructions provided
+2. The options when you select a possible number are shown properly formatted below them
+
+- Select 1 to Place an order
+  - Select 2 to order a burger at $20
+    - Burger ordered successfully
+    - Select 90 to return to the main menu
+    - Select 0 to cancel order
+    - Select 98 to see order history
+    - Select 97 to see current order
+  - Select 3 to order Shawarma at $40
+    - Shawarma ordered successfully
+    - Select 90 to return to the main menu
+    - Select 0 to cancel order
+    - Select 98 to see order history
+    - Select 97 to see current order
+  - Select 4 to order Meat Pie at $10
+    - MeatPie ordered successfully
+    - Select 90 to return to the main menu
+    - Select 0 to cancel order
+    - Select 98 to see order history
+    - Select 97 to see current order
+  - Select 5 to order Pizza at $100
+    - Pizza ordered successfully
+    - Select 90 to return to the main menu
+    - Select 0 to cancel order
+    - Select 98 to see order history
+    - Select 97 to see current order
+  - Select 6 to order our special fries $50
+    - Fries ordered successfully
+    - Select 90 to return to the main menu
+    - Select 0 to cancel order
+    - Select 98 to see order history
+    - Select 97 to see current order
+- Select 98 to see order history
+- Select 97 to see current order
--- utils/instructions.js
+++ utils/instructions.js
@@ -4,14 +4,14 @@ const menu = ["Select 2 to order a burger at $20", "Select 3 to order Shawarma a
 
 const invalidSelection = ["Invalid Selection"]
 
-const burgerOrder = ["Burger orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
+const burgerOrder = ["Burger ordered successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
-const shawarmaOrder = ["Shawarma orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
+const shawarmaOrder = ["Shawarma ordered successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
-const meatPierOrder = ["MeatPie orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
+const meatPierOrder = ["MeatPie ordered successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
-const pizzaOrder = ["Pizza orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
+const pizzaOrder = ["Pizza ordered successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
-const friesOrder = ["Fries orderd successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
+const friesOrder = ["Fries ordered successfully", "Select 90 to return to the main menu", "Select 0 to cancel order",  "Select 98 to see order history", "Select 97 to see current order"]
 
 module.exports = {initial, menu, invalidSelection, burgerOrder, shawarmaOrder, meatPierOrder, pizzaOrder, friesOrder}
